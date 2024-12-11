fd7e81d854607246bc70e7a384f471d0df9cd85c
NafisAhmed174 <nafisahmed947@gmail.com>
2024-12-11 17:09:13 +0600

    added lca + binary lifting

---
+++ Template/Graph Theory/LCA + Binary Lifting.cpp
@@ -0,0 +1,32 @@
+const int LG = 18;
+ 
+vector<int> g[N];
+int par[N][LG + 1], dep[N], sz[N], str[N], en[N], T = 0;
+void dfs(int u, int p = 0) {
+    str[u] = ++T;
+    par[u][0] = p;
+    dep[u] = dep[p] + 1;
+    sz[u] = 1;
+    for (int i = 1; i <= LG; i++) par[u][i] = par[par[u][i - 1]][i - 1];
+    for (auto v: g[u]) if (v != p) {
+        dfs(v, u);
+        sz[u] += sz[v];
+    }
+    en[u] = ++T;
+}
+int lca(int u, int v) {
+    if (dep[u] < dep[v]) swap(u, v);
+    for (int k = LG; k >= 0; k--) if (dep[par[u][k]] >= dep[v]) u = par[u][k];
+    if (u == v) return u;
+    for (int k = LG; k >= 0; k--) if (par[u][k] != par[v][k]) u = par[u][k], v = par[v][k];
+    return par[u][0];
+}
+int kth(int u, int k) {
+    assert(k >= 0);
+    for (int i = 0; i <= LG; i++) if (k & (1 << i)) u = par[u][i];
+    return u;
+}
+int dist(int u, int v) {
+    int l = lca(u, v);
+    return dep[u] + dep[v] - (dep[l] << 1);
+}
