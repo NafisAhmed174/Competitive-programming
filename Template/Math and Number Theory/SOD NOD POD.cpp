90f8925d7448df9553a308d1b68f616fcfe75576
NafisAhmed174 <nafisahmed947@gmail.com>
2024-12-11 18:13:13 +0600

    added SOD NOD POD

---
+++ Template/Math and Number Theory/SOD NOD POD.cpp
@@ -0,0 +1,50 @@
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint=modint<M>;
+
+using mint2=modint<M-1>;
+
+void solve() {
+    ll n=0,a=0,b=0,i=0,j=0,k=0,l=0,r=0,ans=0,temp=0,cnt=0,sum=0;
+    string s,s1,s2,s3;
+    cin>>n;
+    mint x=1,y=1,z=1;
+
+    vpll v(n);
+    cin>>v;
+
+    mint2 ct=1;
+
+    fo(i,n){
+        x*=v[i].ss+1;
+        y*=BigMod<ll>(v[i].ff,v[i].ss+1,M)-1;
+        y/=v[i].ff-1;
+        z=(BigMod<ll>(z.value,v[i].ss+1,M) * BigMod<ll>((BigMod<ll>(v[i].ff,(v[i].ss*(v[i].ss+1)/2),M)),ct.value,M))%M;
+        ct*=v[i].ss+1;
+    }
+    cout<<x<<" "<<y<<" "<<z<<nn; //NOD,SOD and POD
+}
